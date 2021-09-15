042672b7ad17ac20c9e267d5b4f4fe18eb6d037f
Hakan Kenter <hakan.kt@outlook.fr>
2021-09-15 14:58:47 +0200

    Edit

--- src/components/common/Icons.jsx
+++ src/components/common/Icons.jsx
@@ -249,3 +249,60 @@ export const InputSearch = () => {
     </svg>
   );
 };
+
+export const Notification = () => {
+  return (
+    <svg
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d)">
+        <circle cx="30" cy="25" r="20" fill="white" />
+      </g>
+      <path
+        d="M37 26.586V23C37 19.783 34.815 17.073 31.855 16.258C31.562 15.52 30.846 15 30 15C29.154 15 28.438 15.52 28.145 16.258C25.185 17.074 23 19.783 23 23V26.586L21.293 28.293C21.2 28.3857 21.1262 28.4959 21.0759 28.6172C21.0256 28.7386 20.9998 28.8687 21 29V31C21 31.2652 21.1054 31.5196 21.2929 31.7071C21.4804 31.8946 21.7348 32 22 32H38C38.2652 32 38.5196 31.8946 38.7071 31.7071C38.8946 31.5196 39 31.2652 39 31V29C39.0002 28.8687 38.9744 28.7386 38.9241 28.6172C38.8738 28.4959 38.8 28.3857 38.707 28.293L37 26.586ZM37 30H23V29.414L24.707 27.707C24.8 27.6143 24.8738 27.5041 24.9241 27.3828C24.9744 27.2614 25.0002 27.1313 25 27V23C25 20.243 27.243 18 30 18C32.757 18 35 20.243 35 23V27C35 27.266 35.105 27.52 35.293 27.707L37 29.414V30ZM30 35C30.6193 35.0008 31.2235 34.8086 31.7285 34.4502C32.2335 34.0917 32.6143 33.5849 32.818 33H27.182C27.3857 33.5849 27.7665 34.0917 28.2715 34.4502C28.7765 34.8086 29.3807 35.0008 30 35Z"
+        fill="black"
+      />
+      <circle cx="37.5" cy="19.5" r="4.5" fill="#FA3F3F" />
+      <defs>
+        <filter
+          id="filter0_d"
+          x="0"
+          y="0"
+          width="60"
+          height="60"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="5" />
+          <feGaussianBlur stdDeviation="5" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.945098 0 0 0 0 0.94902 0 0 0 0 0.980392 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,31 +1,16 @@
 import React from 'react'
 import './Navbar.scss'
-import {Arrow, ViewArrow, InputSearch} from '../../common/Icons'
+import {Arrow, ViewArrow, InputSearch, Notification} from '../../common/Icons'
 import AccountImage from '../../../assets/img/account-image.png'
+import Profil from './Profil'
+import ConnectedImage from '../../../assets/img/Navbar/orginateur-portable2.jpg'
 
 const Navbar = () => {
     return(
         <div className="navbarMain">
 
             <div className="left">
-                <Arrow />
-                
-                <div className="account">
-                    <img src={AccountImage} alt=""/>
-                    <div className="account-info">
-                        <h4 className="w700 s16">Erza Miller</h4>
-                        <p>erza.miller@email.com</p>
-                    </div>
-                    <ViewArrow />
-                    <p></p>
-                </div>
-
-                <div className="search">
-                    {/* <InputSearch />  */}
-                    <label>
-                        <input type="text" placeholder="Search..." />
-                    </label>
-                </div>
+                <Profil />
             </div>
 
             <div className="right">
@@ -37,3 +22,37 @@ const Navbar = () => {
 }
 
 export default Navbar
+{/* <div className="left">
+    <Arrow />
+    
+    <div className="account">
+        <img src={AccountImage} alt=""/>
+        <div className="account-info">
+            <h4 className="w700 s16">Erza Miller</h4>
+            <p className="w400">erza.miller@email.com</p>
+        </div>
+        <ViewArrow />
+        <p></p>
+    </div>
+
+    <div className="search">
+        <label>
+            <input type="text" placeholder="Search..." />
+        </label>
+    </div>
+</div>
+
+<div className="right">
+    <div className="connected">
+        <p className="point"></p>
+        <p className="online w700 s12">ONLINE</p>
+    </div>
+    <div className="profil">
+        <Profil image={ConnectedImage} position="4" />
+        <Profil image={ConnectedImage} position="3" />
+        <Profil image={ConnectedImage} position="2" />
+        <Profil image={ConnectedImage} position="1" />
+    </div>
+    <p></p>
+    <Notification />
+</div> */}
